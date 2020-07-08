a52762036e34ec2f1028dcfec953398cf2f1a091
abdulelah <abc@example.com>
2020-07-08 09:17:48 +0300

    initial Commit

---
+++ lib/utils/settings.dart
@@ -0,0 +1,2 @@
+// Agora AppId
+const APP_ID = 'b6fccb62f47047458ce83afe99b033e9';
---
+++ signdemo/android/app/src/main/kotlin/com/example/signdemo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.signdemo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
